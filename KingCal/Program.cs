842ae8ad07a32a1944fb8cc2b9c0cc499cdd3585
Heaton <Richard.Heaton@ascension.org>
2019-12-28 12:04:58 -0500

    This commit will add logging.

--- KingCal/Program.cs
+++ KingCal/Program.cs
@@ -1,5 +1,10 @@
+using KingCal.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace KingCal
 {
@@ -7,13 +12,43 @@ namespace KingCal
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.Title = "KingCal Kitchen";
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            //var appSettingsSection = config.GetSection("AppSettings");
+            //var appSettings = appSettingsSection.Get<AppSettings>();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config).CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting Up");
+                Log.Information("******************************************************************");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The application failed to start correctly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseKestrel();
+                    webBuilder.UseSerilog();
                     webBuilder.UseStartup<Startup>();
                 });
     }
